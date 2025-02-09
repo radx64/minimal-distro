bf2f9474f23067cc80d731af6efaaf634e4b59ba
Radek Szewczyk <radekteam@gmail.com>
2025-02-09 18:18:59 +0100

    Add some nice help option. Changed how existence of extracted components is checked (resilent to CTRL+C now). Small visual improvements

--- minimal.sh
+++ minimal.sh
@@ -45,6 +45,30 @@ build_requirements=(
     "syslinux"
 )
 
+distro_dir=$(pwd)/distro
+[ -d ${distro_dir} ] || mkdir ${distro_dir}
+
+help()
+{
+    echo -e "${BLUE} _____ __    ____  "
+    echo -e "|     |  |  |    \ "
+    echo -e "| | | |  |__|  |  |"
+    echo -e "|_|_|_|_____|____/ "
+    echo -e "${ENDC}"
+    echo -e "${GREEN}Minimal Linux Distro${ENDC} build script by ${RED}radx64${ENDC}"
+    echo -e " "
+    echo -e "Usage: ${GREEN}$0${ENDC} [OPTION]"
+    echo -e "Build small bootable Linux distribution from sources."
+    echo -e ""
+    echo -e "Optional arguments:"
+    echo -e "-c, --distclean    Cleans distribution build directory"
+    echo -e "                   removing all downloaded and built artifacts"
+    echo -e "                   Removes: ${RED}${distro_dir}${ENDC}"
+    echo -e "-d, --deps         Checks if host system has all necessary"
+    echo -e "                   packages installed (uses dpkg)"
+    echo -e "-h, --help         Prints this help"
+}
+
 check_dependencies() {
     missing_packages=()
     
@@ -67,28 +91,53 @@ check_dependencies() {
     fi
 }
 
-if [[ "$1" == "--deps" ]]; then
+distclean() {
+    echo -e "Cleaning distro files: ${RED}${distro_dir}${ENDC}"
+    rm -rf ${distro_dir}
+}
+
+print_step() {
+    local step_number="$1"
+    local step_name="$2"
+    
+    local text=" Step ${GREEN}$step_number${ENDC}: $step_name "
+    local control_chars="${GREEN}${ENDC}"
+
+    local text_len=${#text}
+    local control_chars_len=${#control_chars}
+    local plain_text_len=$((text_len - control_chars_len)) 
+    local border=$(printf '%*s' "$plain_text_len" '' | tr ' ' '=')
+    
+    echo ""
+    echo -e "$border"
+    echo -e "$text"
+    echo -e "$border"
+    echo ""
+}
+
+if [[ "$1" == "--deps" ]] || [[ "$1" == "-d" ]]; then
     check_dependencies
+    exit 0
+elif [[ "$1" == "--distclean" ]] || [[ "$1" == "-c" ]]; then
+    distclean
+    exit 0
+elif [[ "$1" == "--help" ]] || [[ "$1" == "-h" ]]; then
+    help
     exit 0
 fi
 
-project_dir=$(pwd)
-
-##temporary for easy clean
-project_dir=$project_dir/temp
-[ -d $project_dir ] || mkdir $project_dir
 
 ### directories
-sources=$project_dir/sources
+sources=$distro_dir/sources
 sources_kernel=$sources/kernel
 sources_userspace=$sources/userspace
 
-build=$project_dir/build
+build=$distro_dir/build
 build_kernel=$build/kernel
 build_userspace=$build/userspace
 
-image=$project_dir/image
-image_initramfs=$project_dir/image/initramfs
+image=$distro_dir/image
+image_initramfs=$distro_dir/image/initramfs
 
 [ -d $sources ] || mkdir $sources
 [ -d $sources_kernel ] || mkdir $sources_kernel
@@ -107,8 +156,10 @@ arch=$(uname -m)
 ### KERNEL BUILD ###
 cd $sources_kernel
 
-if [ -f ${build_kernel}/*/arch/${arch}/boot/bzImage ] ; then
-    echo -e "Do you want to use previously build kernel? (use n if want to rebuild on changed config) [Y/n]:"
+eval "kernel_file=(${build_kernel}/*/arch/${arch}/boot/bzImage)"
+
+if [ -f ${kernel_file} ] ; then
+    echo -e "Do you want to use previously build kernel from ${BLUE}${kernel_file}${ENDC}? (use n if want to rebuild on changed config) [Y/n]:"
     read kernel_choice
     kernel_choice=${kernel_choice:-Y}
 fi
@@ -116,21 +167,21 @@ fi
 if [[ "$kernel_choice" =~ ^[Yy]$ ]]; then
     echo -e "Using already build kernel from ${build_kernel}"
 else
-    echo -e "${WHITE}Step 1.1.${GREEN} Downloading kernel${ENDC}"
+    print_step "1.1" "Downloading kernel"
     wget --no-clobber --show-progress $kernel_source_package
 
-    echo -e "${WHITE}Step 1.2.${GREEN} Extracting kernel${ENDC}"
-    if find ${build_kernel} -mindepth 1 -maxdepth 1 | read; then
+    print_step "1.2" "Extracting kernel"
+    if [ -f  ${build_kernel}/.extracted ] ; then
         echo -e "Extracted kernel found in ${BLUE}${build_kernel}${ENDC} - skipping extraction\n"
     else
         pv *.tar.xz | tar -Jx -C  ${build_kernel}
+        touch ${build_kernel}/.extracted
     fi
 
     cd $build_kernel/linux*
     build_kernel=$(pwd)
 
-    echo -e "${WHITE}Step 1.3.${GREEN} Making default kernel config${ENDC}"
-
+    print_step "1.3" "Making default kernel config"
     if [ -f ${build_kernel}/.config ] ; then
         echo -e "Using existing config found in ${BLUE}${build_kernel}/.config${ENDC} - not generating new one\n"
 
@@ -138,18 +189,20 @@ else
         make defconfig  #try smaller config later (tinyconfig?)
     fi
 
-    echo -e "${WHITE}Step 1.4.${GREEN} Building kernel${ENDC}"
+    print_step "1.4" "Building kernel"
     make -j$(nproc)
 
-    echo -e "${WHITE}Step 1.5.${GREEN} Saving bzImage${ENDC}"
+    print_step "1.5" "Saving bzImage"
     cp arch/$arch/boot/bzImage $image 
 fi
 
 ### USERSPACE BUILD ###
 cd $sources_userspace
 
-if [ -f ${build_userspace}/*/busybox ] ; then
-    echo -e "Do you want to use previously build userspace toolkit? (use n if want to rebuild on changed config) [Y/n]:"
+eval "userspace_file=(${build_userspace}/*/busybox)"
+
+if [ -f ${userspace_file} ] ; then
+    echo -e "Do you want to use previously build userspace toolkit from ${BLUE}${userspace_file}${ENDC}? (use n if want to rebuild on changed config) [Y/n]:"
     read userspace_kernel_choice
     userspace_kernel_choice=${userspace_kernel_choice:-Y}
 fi
@@ -157,14 +210,18 @@ fi
 if [[ "$userspace_kernel_choice" =~ ^[Yy]$ ]]; then
     echo -e "Using already build userspace toolkit from ${build_userspace}"
 else
-    echo -e "${WHITE}Step 2.1.${GREEN} Downloading userspace${ENDC}"
+    print_step "2.1" "Downloading userspace"
     wget --no-clobber --show-progress ${busybox_source_package}
 
-    echo -e "${WHITE}Step 2.2.${GREEN} Extracting userspace${ENDC}"
-    pv *.tar.bz2 | tar -xjv -C ${build_userspace} -f -
+    if [ -f  ${build_userspace}/.extracted ] ; then    
+        echo -e "Extracted userspace found in ${BLUE}${build_userspace}${ENDC} - skipping extraction\n"
+    else
+        print_step "2.2" "Extracting userspace"
+        pv *.tar.bz2 | tar -xjv -C ${build_userspace} -f -
+        touch ${build_userspace}/.extracted
+    fi
 
-    echo -e "${WHITE}Step 2.3.${GREEN} Making default userspace config${ENDC}"
-
+    print_step "2.3" "Making default userspace config"
     cd ${build_userspace}/busybox*
     build_userspace=$(pwd)
 
@@ -175,23 +232,23 @@ else
         make defconfig
     fi
 
-    echo -e "${WHITE}Step 2.4.${GREEN} Switching to static linking${ENDC}"
+    print_step "2.4" "Switching to static linking"
     sed 's/^.*CONFIG_STATIC.*$/CONFIG_STATIC=y/' -i .config # for static linking
     sed 's/^CONFIG_MAN=y/CONFIG_MAN=n/' -i .config  # no manual pages
     echo "CONFIG_STATIC_LIBGCC=y" >> .config   # configure static libgcc
 
-    echo -e "${WHITE}Step 2.5.${GREEN} Fixing of busybox traffic control related symbols ${ENDC}"
+    print_step "2.5" "Fixing of busybox traffic control related symbols"
     sed 's/^CONFIG_TC=y/CONFIG_TC=n/' -i .config    # some newer kernels are not providing traffic control defines anymore
 
-    echo -e "${WHITE}Step 2.6.${GREEN} Building userspace${ENDC}"
+    print_step "2.6" "Building userspace"
     make -j$(nproc)
 
-    echo -e "${WHITE}Step 2.7.${GREEN} Installing userspace${ENDC}"
+    print_step "2.7" "Installing userspace"
     make CONFIG_PREFIX=${image_initramfs} install
     rm ${image_initramfs}/linuxrc
 fi
 
-echo -e "${WHITE}Step 3.1.${GREEN} Creating init script${ENDC}"
+print_step "3.1" "Creating init script"
 cd ${image_initramfs}
 
 cat << EOF > ./init
@@ -204,25 +261,24 @@ ln -sf /dev/null /dev/tty4
 /bin/sh
 EOF
 
-echo -e "${WHITE}Step 3.2.${GREEN} Marking init script as executable${ENDC}"
+print_step "3.2" "Marking init script as executable"
 chmod +x ./init
 
-echo -e "${WHITE}Step 3.3.${GREEN} Creating initramfs${ENDC}"
+print_step "3.3" "Creating initramfs"
 find . | cpio -o -H newc > ${image}/initramfs.cpio
 
 ### IMAGE CREATION ###
-echo -e "${WHITE}Step 4.1.${GREEN} Creating boot image${ENDC}"
+print_step "4.1" "Creating boot disk image"
 cd ${image}
 dd if=/dev/zero of=./boot.img bs=1M count=64
 
-echo -e "${WHITE}Step 4.2.${GREEN} Creating file system for boot image${ENDC}"
+print_step "4.2" "Creating file system for boot disk image"
 mkfs -t fat boot.img
 
-echo -e "${WHITE}Step 4.3.${GREEN} Installing syslinux bootloader ${ENDC}"
+print_step "4.3" "Installing syslinux bootloader"
 syslinux ./boot.img
 
-echo -e "${WHITE}Step 4.4.${GREEN} Creating syslinux bootloader config${ENDC}"
-
+print_step "4.4" "Creating syslinux bootloader config"
 cat << EOF > ./syslinux.cfg
 DEFAULT minimal
 PROMPT 0        # Set to 1 to display boot: prompt
@@ -234,7 +290,7 @@ LABEL minimal
 EOF
 
 # TODO consider different method of boot image creation that does not require root privileges
-echo -e "${WHITE}Step 4.5.${GREEN} Copying kernel and initramfs to boot image ${ENDC}"
+print_step "4.5" "Copying kernel and initramfs to boot image (root privileges required)"
 [ -d boot_mount ] || mkdir boot_mount
 sudo mount boot.img boot_mount
 sudo cp {bzImage,syslinux.cfg,initramfs.cpio} boot_mount
@@ -242,5 +298,5 @@ sudo umount boot_mount
 rm boot_mount -rf
 
 ### RUN ###
-echo -e "${WHITE}Step 4.6.${RED} Starting qemu with kernel and initramfs packed in boot.img${ENDC}"
+print_step "5.1" "Starting qemu with kernel and initramfs packed in boot.img"
 qemu-system-x86_64 -drive format=raw,file=./boot.img
