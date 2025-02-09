634260fcfee6c13177a3692818d835c609d172d8
Radek Szewczyk <radekteam@gmail.com>
2025-02-09 14:48:22 +0100

    Implements reusability of already build / downloaded kernel

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # minimal-distro
 Minimal Linux Distro experiment
+
+This script fetches Linux kernel and busybox, builds them and prepare some small disk image that is booted in qemu.
--- minimal.sh
+++ minimal.sh
@@ -15,7 +15,7 @@ RED="\e[91m"
 GREEN="\e[92m"
 BLUE="\e[94m"
 WHITE="\e[97m"
-ENDCOLOR="\e[0m"
+ENDC="\e[0m"
 
 build_requirements=(
 # kernel
@@ -28,7 +28,6 @@ build_requirements=(
     "make"
     "openssl"
     "pv"
-    "qemu-system-x86"
     "tar"
     "wget"
 
@@ -37,6 +36,9 @@ build_requirements=(
 
 # initramfs
     "cpio"
+
+# qemu
+    "qemu-system-x86"
 
 # diskimage
     "dosfstools"
@@ -49,9 +51,9 @@ check_dependencies() {
     for pkg in "${build_requirements[@]}"; do
         version=$(dpkg-query -W -f='${Package} ${Version}\n' 2>/dev/null | grep -E "^$pkg(:[a-z0-9]+)? " | awk '{print $2}')
         if [[ -n "$version" ]]; then
-            echo -e "  [ ${GREEN}OK${ENDCOLOR} ] $pkg at version ${BLUE}$version${ENDCOLOR}"
+            echo -e "  [ ${GREEN}OK${ENDC} ] $pkg at version ${BLUE}$version${ENDC}"
         else
-            echo -e "  [${RED}FAIL${ENDCOLOR}] $pkg"
+            echo -e "  [${RED}FAIL${ENDC}] $pkg"
             missing_packages+=("$pkg")
         fi
     done
@@ -102,54 +104,75 @@ image_initramfs=$project_dir/image/initramfs
 ### KERNEL BUILD ###
 cd $sources_kernel
 
-echo -e "${WHITE}Step 1.1.${GREEN} Downloading kernel${ENDCOLOR}"
-wget --show-progress $kernel_source_package
+if [ -f ${build_kernel}/bzImage ] ; then
+    echo -e "Do you want to use previously build kernel? [Y/n]:"
+    read answer
+    answer=${answer:-Y}
+fi
 
-echo -e "${WHITE}Step 1.2.${GREEN} Extracting kernel${ENDCOLOR}"
-pv *.tar.xz | tar -Jx -C $build_kernel
+if [[ "$choice" =~ ^[Yy]$ ]]; then
+    echo -e "Using already build kernel from ${build_kernel}"
+else
+    echo -e "${WHITE}Step 1.1.${GREEN} Downloading kernel${ENDC}"
+    wget --no-clobber --show-progress $kernel_source_package
 
-cd $build_kernel/linux*
-build_kernel=$(pwd)
+    echo -e "${WHITE}Step 1.2.${GREEN} Extracting kernel${ENDC}"
+    if find ${build_kernel} -mindepth 1 -maxdepth 1 | read; then
+        echo -e "Extracted kernel found in ${BLUE}${build_kernel}${ENDC} - skipping extraction\n"
+    else
+        pv *.tar.xz | tar -Jx -C  ${build_kernel}
+    fi
 
-echo -e "${WHITE}Step 1.3.${GREEN} Making default kernel config${ENDCOLOR}"
-make defconfig  #try smaller config later (tinyconfig?)
+    cd $build_kernel/linux*
+    build_kernel=$(pwd)
 
-echo -e "${WHITE}Step 1.4.${GREEN} Building kernel${ENDCOLOR}"
-make -j$(nproc)
+    echo -e "${WHITE}Step 1.3.${GREEN} Making default kernel config${ENDC}"
 
-echo -e "${WHITE}Step 1.5.${GREEN} Saving bzImage${ENDCOLOR}"
-arch=$(uname -m)
-[ $arch = 'i686' ] && arch="i386" # hack i686 to i386 packages :D
-cp arch/$arch/boot/bzImage $image 
+    if [ -f ${build_kernel}/.config ] ; then
+        echo -e "Using existing config found in ${BLUE}${build_kernel}/.config${ENDC} - not generating new one\n"
+
+    else
+        make defconfig  #try smaller config later (tinyconfig?)
+    fi
+
+    echo -e "${WHITE}Step 1.4.${GREEN} Building kernel${ENDC}"
+    make -j$(nproc)
+
+    echo -e "${WHITE}Step 1.5.${GREEN} Saving bzImage${ENDC}"
+    arch=$(uname -m)
+    [ $arch = 'i686' ] && arch="i386" # hack i686 to i386 packages :D
+    cp arch/$arch/boot/bzImage $image 
+fi
+
 
 ### USERSPACE BUILD ###
 cd $sources_userspace
-echo -e "${WHITE}Step 2.1.${GREEN} Downloading userspace${ENDCOLOR}"
-wget --show-progress ${busybox_source_package}
+echo -e "${WHITE}Step 2.1.${GREEN} Downloading userspace${ENDC}"
+wget --no-clobber --show-progress ${busybox_source_package}
 
-echo -e "${WHITE}Step 2.2.${GREEN} Extracting userspace${ENDCOLOR}"
+echo -e "${WHITE}Step 2.2.${GREEN} Extracting userspace${ENDC}"
 pv *.tar.bz2 | tar -xjv -C ${build_userspace} -f -
 
-echo -e "${WHITE}Step 2.3.${GREEN} Making default userspace config${ENDCOLOR}"
+echo -e "${WHITE}Step 2.3.${GREEN} Making default userspace config${ENDC}"
 cd ${build_userspace}/busybox*
 make defconfig
 
-echo -e "${WHITE}Step 2.4.${GREEN} Switching to static linking${ENDCOLOR}"
-sed 's/^.*CONFIG_STATIC.*$/CONFIG_STATIC=y/' -i .config
-sed 's/^CONFIG_MAN=y/CONFIG_MAN=n/' -i .config
+echo -e "${WHITE}Step 2.4.${GREEN} Switching to static linking${ENDC}"
+sed 's/^.*CONFIG_STATIC.*$/CONFIG_STATIC=y/' -i .config # for static linking
+sed 's/^CONFIG_MAN=y/CONFIG_MAN=n/' -i .config  # no manual pages
 echo "CONFIG_STATIC_LIBGCC=y" >> .config   ## this might be no longer needed? Need to check busybox sources
 
-echo -e "${WHITE}Step 2.5.${GREEN} Fixing of busybox traffic control related symbols ${ENDCOLOR}"
-sed 's/^CONFIG_TC=y/CONFIG_TC=n/' -i .config
+echo -e "${WHITE}Step 2.5.${GREEN} Fixing of busybox traffic control related symbols ${ENDC}"
+sed 's/^CONFIG_TC=y/CONFIG_TC=n/' -i .config    # some newer kernels are not providing traffic control defines anymore
 
-echo -e "${WHITE}Step 2.6.${GREEN} Building userspace${ENDCOLOR}"
+echo -e "${WHITE}Step 2.6.${GREEN} Building userspace${ENDC}"
 make -j$(nproc)
 
-echo -e "${WHITE}Step 2.7.${GREEN} Installing userspace${ENDCOLOR}"
+echo -e "${WHITE}Step 2.7.${GREEN} Installing userspace${ENDC}"
 make CONFIG_PREFIX=${image_initramfs} install
 rm ${image_initramfs}/linuxrc
 
-echo -e "${WHITE}Step 3.1.${GREEN} Creating init script${ENDCOLOR}"
+echo -e "${WHITE}Step 3.1.${GREEN} Creating init script${ENDC}"
 cd ${image_initramfs}
 
 cat << EOF > ./init
@@ -162,29 +185,29 @@ ln -sf /dev/null /dev/tty4
 /bin/sh
 EOF
 
-echo -e "${WHITE}Step 3.2.${GREEN} Marking init script as executable${ENDCOLOR}"
+echo -e "${WHITE}Step 3.2.${GREEN} Marking init script as executable${ENDC}"
 chmod +x ./init
 
-echo -e "${WHITE}Step 3.3.${GREEN} Creating initramfs${ENDCOLOR}"
+echo -e "${WHITE}Step 3.3.${GREEN} Creating initramfs${ENDC}"
 find . | cpio -o -H newc > ${image}/initramfs.cpio
 
-# echo -e "${WHITE}Step 3.4.${RED} Running temporary qemu to test bzImage and initramfs! Still flaky and full of warnings :D${ENDCOLOR}"
+# echo -e "${WHITE}Step 3.4.${RED} Running temporary qemu to test bzImage and initramfs! Still flaky and full of warnings :D${ENDC}"
 # ### CTRL + a x to kill nographic qemu
 #cd ${image}
 #qemu-system-x86_64 --kernel ${image}/bzImage --initrd ${image}/initramfs.cpio
 
 ### IMAGE CREATION ###
-echo -e "${WHITE}Step 4.1.${GREEN} Creating boot image${ENDCOLOR}"
+echo -e "${WHITE}Step 4.1.${GREEN} Creating boot image${ENDC}"
 cd ${image}
 dd if=/dev/zero of=./boot.img bs=1M count=64
 
-echo -e "${WHITE}Step 4.2.${GREEN} Creating file system for boot image${ENDCOLOR}"
+echo -e "${WHITE}Step 4.2.${GREEN} Creating file system for boot image${ENDC}"
 mkfs -t fat boot.img
 
-echo -e "${WHITE}Step 4.3.${GREEN} Installing syslinux bootloader ${ENDCOLOR}"
+echo -e "${WHITE}Step 4.3.${GREEN} Installing syslinux bootloader ${ENDC}"
 syslinux ./boot.img
 
-echo -e "${WHITE}Step 4.3.${GREEN} Creating syslinux bootloader config${ENDCOLOR}"
+echo -e "${WHITE}Step 4.4.${GREEN} Creating syslinux bootloader config${ENDC}"
 
 cat << EOF > ./syslinux.cfg
 DEFAULT minimal
@@ -193,12 +216,11 @@ TIMEOUT 50
 LABEL minimal
     MENU LABEL Minimal Linux
     LINUX ../bzImage 
-    APPEND root=/dev/sda1 rw
     INITRD ../initramfs.cpio
 EOF
 
 # TODO consider different method of boot image creation that does not require root privileges
-echo -e "${WHITE}Step 4.4.${GREEN} Copying kernel and initramfs to boot image ${ENDCOLOR}"
+echo -e "${WHITE}Step 4.5.${GREEN} Copying kernel and initramfs to boot image ${ENDC}"
 [ -d boot_mount ] || mkdir boot_mount
 sudo mount boot.img boot_mount
 sudo cp {bzImage,syslinux.cfg,initramfs.cpio} boot_mount
@@ -206,5 +228,5 @@ sudo umount boot_mount
 rm boot_mount -rf
 
 ### RUN ###
-echo -e "${WHITE}Step 4.5.${RED} Starting qemu with kernel and initramfs packed in boot.img${ENDCOLOR}"
+echo -e "${WHITE}Step 4.6.${RED} Starting qemu with kernel and initramfs packed in boot.img${ENDC}"
 qemu-system-x86_64 -drive format=raw,file=./boot.img
